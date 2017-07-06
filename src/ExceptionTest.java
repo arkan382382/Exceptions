aa37e8264b36f64863edb4455abcff489a3f009a
arkan382382 <arkan382382@outlook.com>
2017-07-06 13:54:33 +0200

    Update ExceptionTest.java

--- src/ExceptionTest.java
+++ src/ExceptionTest.java
@@ -49,3 +49,32 @@ class Matrix{
         return result[0];
     }
 }
+
+/*      Example p. 68 ClearCode
+   try {   
+       deletePage(page);   
+       registry.deleteReference(page.name);   
+       configKeys.deleteKey(page.name.makeKey()); 
+   } catch (Exception e) {   
+       logger.log(e.getMessage()); 
+   } 
+}
+
+        versus
+    public void delete(Page page) {   
+        try {      
+            deletePageAndAllReferences(page);   
+        }   
+        catch (Exception e) {      
+            logError(e);   
+        }
+     }
+     private void deletePageAndAllReferences(Page page) throws Exception {   
+        deletePage(page);   
+        registry.deteleReference(page.name);   
+        configKeys.deleteKey(page.name.makeKey()); 
+     }
+     private void logError(Exception e){   
+        logger.log(e.getMessage()); 
+     }
+*/
